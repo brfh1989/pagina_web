1e6ea147fc266b516975ea256f9c905e6fd5053b
brfh1989 <127067288+brfh1989@users.noreply.github.com>
2024-08-25 01:14:54 -0600

    realizado

---
+++ pagina_web.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Título de la página
+st.title("MI PRIMERA PÁGINA WEB")
+
+# Contenido de la página
+st.write("**HOLA ESTA ES MI PAGINA WEB LOS QUIERO MUCHO**")
+st.write("**BRUNO**")
+st.write("ESTOY HACIENDO MI PAGINA WEB QUE EMOCION")
+st.write("**BUENO Y AHORA QUE HAGO**")
+st.write("SI")
+
+# Pie de página
+st.write("©Todos los derechos reservados 2019")
